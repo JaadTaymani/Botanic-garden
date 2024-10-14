b5fefa3e6ac7db6c538fb738d51ccafbdf8e437c
Jaad Taymani <116976865+JaadTaymani@users.noreply.github.com>
2024-10-14 22:58:43 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,30 @@
+from sklearn.metrics import confusion_matrix
+import itertools
 
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras.optimizers import RMSprop,Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+model = Sequential()
+#
+model.add(Conv2D(filters = 8, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu', input_shape = (28,28,1)))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+#
+model.add(Conv2D(filters = 16, kernel_size = (3,3),padding = 'Same', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+model.add(Dropout(0.25))
+# fully connected
+model.add(Flatten())
+model.add(Dense(256, activation = "relu"))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = "softmax"))
+
+optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
+
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
